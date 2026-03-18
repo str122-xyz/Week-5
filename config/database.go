6b064c9874f44ff7a72f418e11c9ade4cd47355f
Satriahrl_ <satriaherlambang22@gmail.com>
2026-03-18 18:13:31 +0700

    feat: menambahkan konfigurasi database

---
+++ config/database.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/str122-xyz/gin-firebase-backend/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func InitDatabase() {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		user, password, host, port, dbname)
+
+	gormConfig := &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	}
+
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), gormConfig)
+	if err != nil {
+		log.Fatalf("Gagal koneksi ke database: %v", err)
+	}
+
+	sqlDB, err := DB.DB()
+	if err != nil {
+		log.Fatalf("Gagal mendapatkan sql.DB: %v", err)
+	}
+	
+	sqlDB.SetMaxOpenConns(25)
+	sqlDB.SetMaxIdleConns(10)
+
+	err = DB.AutoMigrate(
+		&models.User{},
+		&models.Product{},
+	)
+	if err != nil {
+		log.Fatalf("AutoMigrate gagal: %v", err)
+	}
+	
+	log.Println("Database terhubung dan tabel sudah di-migrate")
+}
