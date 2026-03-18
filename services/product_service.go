13604c1ad69a1d0cf0df3c7aaae967fb62c6e743
Satriahrl_ <satriaherlambang22@gmail.com>
2026-03-18 19:37:00 +0700

    get by all product dan get by id

--- services/product_service.go
+++ services/product_service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"github.com/str122-xyz/gin-firebase-backend/models"
 	"github.com/str122-xyz/gin-firebase-backend/repositories"
 )
 
@@ -10,4 +11,18 @@ type ProductService struct {
 
 func NewProductService() *ProductService {
 	return &ProductService{productRepo: repositories.NewProductRepository()}
+}
+
+func (s *ProductService) GetAll(page, limit int, category string) ([]models.Product, int64, error) {
+	if page <= 0 {
+		page = 1
+	}
+	if limit <= 0 || limit > 100 {
+		limit = 10
+	}
+	return s.productRepo.FindAll(page, limit, category)
+}
+
+func (s *ProductService) GetByID(id uint) (*models.Product, error) {
+	return s.productRepo.FindByID(id)
 }
