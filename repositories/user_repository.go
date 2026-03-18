dda0c957573f13fc91243892c3ff13f9bca7de12
Satriahrl_ <satriaherlambang22@gmail.com>
2026-03-18 18:53:40 +0700

    find by firebase uid

--- repositories/user_repository.go
+++ repositories/user_repository.go
@@ -1,7 +1,22 @@
 package repositories
 
+import (
+	"github.com/str122-xyz/gin-firebase-backend/config"
+	"github.com/str122-xyz/gin-firebase-backend/models"
+)
+
 type UserRepository struct{}
 
 func NewUserRepository() *UserRepository {
 	return &UserRepository{}
+}
+
+// FindByFirebaseUID mencari user berdasarkan Firebase UID
+func (r *UserRepository) FindByFirebaseUID(uid string) (*models.User, error) {
+	var user models.User
+	result := config.DB.Where("firebase_uid = ?", uid).First(&user)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &user, nil
 }
